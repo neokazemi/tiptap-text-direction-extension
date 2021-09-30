dfdbfcad8e10cb37fad2bba5357e1af38209a8d2
Neo <neo.kazemi168@gmail.com>
2021-09-30 16:20:10 +0330

    version 1.0.0

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import { Extension } from '@tiptap/core';
+
+const TextDirection = Extension.create({
+    name: 'textDirection',
+    defaultOptions: {
+        types: ['heading', 'paragraph'],
+        directions: ['ltr', 'rtl', 'auto'],
+        defaultDirection: 'auto',
+    },
+    addGlobalAttributes() {
+        return [
+            {
+                types: this.options.types,
+                attributes: {
+                    dir: {
+                        default: this.options.defaultDirection,
+                        parseHTML: element => ({
+                            dir: element.attributes['dir'].value || this.options.defaultDirection,
+                        }),
+                        renderHTML: attributes => {
+                            if (attributes.dir === this.options.defaultDirection) {
+                                return {}
+                            }
+
+                            return { dir: attributes.dir }
+                        },
+                    },
+                },
+            },
+        ];
+    },
+    addCommands() {
+        return {
+            setTextDirection: (direction) => ({ commands }) => {
+                if (!this.options.directions.includes(direction)) {
+                    return false
+                }
+                return this.options.types.every(type => commands.updateAttributes(type, { dir: direction }))
+            },
+            unsetTextDirection: () => ({ commands }) => {
+                return this.options.types.every(type => commands.resetAttributes(type, 'dir'))
+            },
+        }
+    },
+});
+
+export default TextDirection;
+export { TextDirection };
